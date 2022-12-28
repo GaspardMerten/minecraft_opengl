23ca296b4ebe255229e38018f6371c27986bfae0
Gaspard Merten <gaspard.mp.work@gmail.com>
2022-12-28 23:40:16 +0100

    Finishing camera zoom

--- src/controls/player_controls/PlayerControls.cpp
+++ src/controls/player_controls/PlayerControls.cpp
@@ -10,28 +10,41 @@ PlayerControls::PlayerControls(Transform &transform, Camera &camera, World &worl
 void PlayerControls::processEvents(GLFWwindow *window) {
     glm::vec3 oldPosition = this->transform.position;
 
-    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
-        transform.translate(-1, 0);
-    }
-    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
-        transform.translate(1, 0);
-    }
-    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-        transform.translate(0, 0, -1);
-    }
-    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-        transform.translate(0, 0, 1);
-    }
-    // space to jump
-    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
-        transform.translate(0, 1, 0);
-    }
-    // shift to go down
-    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
-        transform.translate(0, -1, 0);
+    if (glfwGetKey(window, GLFW_KEY_LEFT_ALT) != GLFW_PRESS) {
+
+        if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+            transform.translate(-1, 0);
+        }
+        if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+            transform.translate(1, 0);
+        }
+        if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+            transform.translate(0, 0, -1);
+        }
+        if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+            transform.translate(0, 0, 1);
+        }
+        // space to jump
+        if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+            transform.translate(0, 1, 0);
+        }
+        // shift to go down
+        if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
+            transform.translate(0, -1, 0);
+        }
     }
 
-    if (world.worldBlocks[std::tuple<int, int, int>(transform.position.x, transform.position.z, transform.position.y)] == 1) {
+    // rotate if pressing left alt
+    if (glfwGetKey(window, GLFW_KEY_LEFT_ALT) == GLFW_PRESS) {
+        if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+            transform.rotateY(0.1);
+        } else if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+            transform.rotateY(-0.1);
+        }
+    }
+
+    if (world.worldBlocks[std::tuple<int, int, int>(transform.position.x, transform.position.z,
+                                                    transform.position.y)] == 1) {
         transform.position = oldPosition;
     }
 }
